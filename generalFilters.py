384d8871eda86b1e68d8a0aedaa04f06b0ad43bb
JoseNaime <josep.naimeg@gmail.com>
2021-03-18 17:41:02 -0600

    feat(filtes): bgrFilter & epicFilter added to class, filter files deleted

--- generalFilters.py
+++ generalFilters.py
@@ -21,3 +21,38 @@ class Filters:
         finalImg[:, :, 2] = img[:, :, 2] * d
 
         return finalImg
+
+    """
+        img: Image to apply filter
+        r: Read value
+        g: Green value
+        b: Blue value
+    """
+    @staticmethod
+    def addBgrFilter(img, r, g, b):
+        filter = np.full(img.shape, (b, g, r), np.uint8)
+        finalImg = cv.add(img, filter)
+        finalImg = cv.addWeighted(finalImg, 0.8, filter, 0.2, 0)
+
+        return finalImg
+
+    """
+        img: Image to apply filter
+        filterAmount: Amount of filter applied to image (0 - 1)
+        filterSaturation: 
+        gamma: Add gamma to final (0 - 255)
+    """
+    @staticmethod
+    def addEpicFilter(img, filterAmount, filterSaturation, gamma):
+        redImg = cv.addWeighted(img, abs(filterSaturation - 1),
+                                np.full((img.shape[0], img.shape[1], 3), (255, 0, 0), np.uint8),
+                                filterSaturation, 0)
+        blueImg = cv.addWeighted(img, abs(filterSaturation - 1),
+                                 np.full((img.shape[0], img.shape[1], 3), (0, 0, 255), np.uint8),
+                                 filterSaturation, 0)
+
+        finalImg = cv.addWeighted(img, abs(filterAmount - 1) + .2, cv.addWeighted(redImg, 0.5, blueImg, 0.5, .5),
+                                  filterAmount,
+                                  gamma)
+
+        return finalImg
