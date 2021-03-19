7c1c47107cd72c1c26838f566adf4972a6c21ad8
CarlosCRG19 <carloscrg19@gmail.com>
2021-03-18 20:13:49 -0600

    feat: basic filters added

--- main.py
+++ main.py
@@ -1,67 +1,91 @@
 import cv2
+import copy
+import numpy as np
 import tkinter as tk 
 from PIL import Image, ImageTk
 from tkinter import filedialog as fd 
 
-def upload_image():
-    """
-    Function called by the Browse button. It opens the file explorer and asks you to select a png or jpeg file 
-    (actually, only this type of files are displayed on the file explorer). 
-    After that, it reads the file and stores it in the image variable.
-    """
-    global image # this variable now belongs to the global scope
+# ------------------
+# -- FILTERS CODE --
+# ------------------
 
-    select_text.set("Loading...") 
-    filepath = fd.askopenfile(filetypes=(('image files', '.png'), ('image files', '.jpg'))) # filedialog function to open a file, it will only accept png or jpg images
-    if filepath:
-        select_text.set('Loaded')
-        print('image successfully loaded')
-        image = cv2.imread(filepath.name)
+sharp = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+edges = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
 
-        # create the image object, and save it so that it
-        # won't get deleted by the garbage collector
-        canvas.image_tk = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(image, maxsize)))
+# Basic Filters
+def basic_filters(filter):
+    global modifiable_image
 
-        # configure the canvas item to use this image
-        canvas.itemconfigure(image_id, image=canvas.image_tk)
+    if filter == 'blur':
+        modifiable_image = cv2.blur(modifiable_image,(5,5))
+    else:
+        modifiable_image  = cv2.filter2D(modifiable_image, -1, filter)
 
+    canvas.image_tk = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(modifiable_image , maxsize)))
+    # configure the canvas item to use this image
+    canvas.itemconfigure(image_id, image=canvas.image_tk)
 
+# --------------
+# -- GUI CODE --
+# --------------
 
-# image variables
-image = None
-maxsize = (300, 350)
+# Interface and Image Uploading
+# -----------------------------
 
 # interface
 root = tk.Tk()
-root.geometry('300x350')
+root.geometry('400x400')
 root.title('Awesome Image Filters')
-root.resizable(True, True)
 
+# canvas creation
 canvas = tk.Canvas(root)
-canvas.pack(side=tk.BOTTOM, padx=15, pady=15)
+canvas.grid(column=0, row=0, columnspan=3)
 
-# instructions 
-
-# browse button and image loading
+# image variables
+image = None
+modifiable_image = None # copy of main image so it can be changed without altering the latter
+maxsize = (350, 250)
 image_id = canvas.create_image(20, 20, anchor='nw')
 
-select_text = tk.StringVar()
-selected_image = tk.Button(root, command=upload_image, textvariable=select_text, font='Raleway') # when the button is pressed, it opens the file explorer
-select_text.set('Select Image')
-selected_image.pack(side=tk.LEFT, padx=35)
+# instructions
+instructions_text = tk.StringVar()
+instructions = tk.Label(root, textvariable=instructions_text, font='Raleway')
+instructions_text.set('Select a PNG or a JPEG file')
+instructions.grid(column=0, row=2, columnspan=3)
+
+# browse button and image loading
+browse_text = tk.StringVar()
+browse_image = tk.Button(root, width=12, height=1, command=upload_image, textvariable=browse_text, font='Raleway') # when the button is pressed, it opens the file explorer
+browse_text.set('Select Image')
+browse_image.grid(column=0, row=3)
+root.grid_rowconfigure(1, minsize=10)
+
+# save image button
+save_button = tk.Button(root, width=10, height=1, text='Save Image', font='Raleway')
+save_button.grid(column=1, row=3)
 
 # exit button 
 exit_button = tk.Button(root, command=lambda: exit(), text='Exit', font='Raleway')
-exit_button.pack(side=tk.LEFT)
+exit_button.grid(column=2, row=3)
 
+
+# Menu and filter application 
+# ---------------------------
+
+# main menu
 menu = tk.Menu(root) 
 root.config(menu=menu) 
-root.title('Image Lab') 
-basicFilterMenu = tk.Menu(menu)
-menu.add_cascade(label='Basic Filters', menu=basicFilterMenu) 
-basicFilterMenu.add_command(label='Sharpen') 
-basicFilterMenu.add_command(label='Blur') 
-basicFilterMenu.add_command(label='Show edges')
+
+# basic filter menu
+basic_filters_menu = tk.Menu(menu)
+menu.add_cascade(label='Basic Filters', menu=basic_filters_menu) 
+
+basic_filters_menu.add_command(label='Sharpen', command=lambda:basic_filters(sharp)) 
+basic_filters_menu.add_command(label='Blur', command=lambda:basic_filters('blur')) 
+basic_filters_menu.add_command(label='Show edges', command=lambda:basic_filters(edges))
+
+# color filter menu
+
 colorFilterMenu = tk.Menu(menu)
 menu.add_cascade(label='Color Filters', menu=colorFilterMenu) 
 colorFilterMenu.add_command(label='Red tint')
@@ -71,9 +95,3 @@ colorFilterMenu.add_command(label='Custom tint...')
 
 root.mainloop() 
 
-
-
-
-
-
-root.mainloop()
